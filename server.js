1b8d8d6a9a8b1857a9e756bec3fb05113c876f37
eldoryaqubovebek3094 <148733238+eldoryaqubovebek3094@users.noreply.github.com>
2024-02-19 20:54:23 +0300

    lol

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const serverless = require('serverless-http');
-
+console.log('salom')
 app.get('/', (req, res) => {
+    
   res.send('Hello, Netlify!');
 });
 
