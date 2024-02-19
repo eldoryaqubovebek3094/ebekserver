e3874863c594222012c3c90898741fc6bd127456
eldoryaqubovebek3094 <148733238+eldoryaqubovebek3094@users.noreply.github.com>
2024-02-19 21:06:04 +0300

    ishlandi

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 const serverless = require('serverless-http');
-console.log('salom')
+const port = 3000;
+const data = [
+  {
+    name: "Yaqubov Eldor",
+    email: "ebek3094@gmail.com",
+    message: "web master",
+    phoneNumber: "+998992790680",
+    amount: "7200000",
+    image: "Screenshot 2024-02-02 184749.png",
+  }
+];
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/getAllUsers", (req, res) => {
+  res.json(data);
+});
+app.post("/addNewUser", (req, res) => {
+  data.push(req.body);
+  res.send("User added successfully");
+});
 app.get('/', (req, res) => {
     
   res.send('Hello, Netlify!');
@@ -9,3 +28,6 @@ app.get('/', (req, res) => {
 
 module.exports = app;
 module.exports.handler = serverless(app);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
