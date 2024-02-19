30f409bb972b49cdea4f93230273c7efd24ce5a9
eldoryaqubovebek3094 <148733238+eldoryaqubovebek3094@users.noreply.github.com>
2024-02-19 20:44:05 +0300

    boss

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
+const serverless = require('serverless-http');
 
 app.get('/', (req, res) => {
   res.send('Hello, Netlify!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+module.exports = app;
+module.exports.handler = serverless(app);
